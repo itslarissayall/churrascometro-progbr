a598d66d20db815ec5d290e5c5c8ee3feae55543
Larissa Santos <larissasantos.dev@gmail.com>
2022-06-19 00:28:54 -0300

    refactoring code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function calc() {
-    let adultsValue = document.getElementById("adults").value;
-    let kidsValue = document.getElementById("kids").value;
+    let adults = document.getElementById("adults").value;
+    let kids = document.getElementById("kids").value;
     let time = document.getElementById("time").value;
     let mMT = 450;
     let mBR = 1200;
@@ -11,11 +11,11 @@ function calc() {
     let meat = document.getElementById("meat");
     let beer = document.getElementById("beer");
     let soda = document.getElementById("soda");
-    adultsValue && kidsValue && time > 0 ?
-        ((time > 6 ? (mCRN += 200, mCVJ += 800, mRA += 500) : (false)),
-            rMT = (mMT * adultsValue) + (mMT * 0.5 * kidsValue),
-            rBR = mBR * (adultsValue),
-            rSD = (mSD * adultsValue) + (mSD * 0.5 * kidsValue),
+    adults && kids && time > 0 ?
+        ((time > 6 ? (mMT += 200, mBR += 800, mSD += 500) : (false)),
+            rMT = (mMT * adults) + (mMT * 0.5 * kids),
+            rBR = mBR * (adults),
+            rSD = (mSD * adults) + (mSD * 0.5 * kids),
             meat.innerHTML = "A quantidade necessária de carne é \n" + (rMT/ 1000).toFixed(2) + "\n kg",
             beer.innerHTML = "A quantidade necessária de cerveja é \n" + (rBR / 1000).toFixed(2) + "\n litros",
             soda.innerHTML = "A quantidade necessária de refrigerante e água é \n" + (rSD / 1000).toFixed(2) + "\n litros",
@@ -33,7 +33,7 @@ function showModal() {
 function closeModal() {
     let modal = document.getElementById("modal-result");
     modal.classList.toggle("close-modal");
-    adultsValue.value = "";
-    kidsValue.value = "";
+    adults.value = "";
+    kids.value = "";
     time.value = "";
 }
